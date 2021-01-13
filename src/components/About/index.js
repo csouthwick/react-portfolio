e829f143be16b6e36140aa55f4c01954d81b7160
Chris Southwick <csouthwick@live.com>
2021-01-13 02:54:32 -0500

    adjusted styles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import photo from '../../assets/images/photo.jpg';
 function About() {
   return (
-    <section className="my-5">
-      <h2 id="about">About Me</h2>
-      <img src={photo} className="my-2" alt="Chris Southwick" />
+    <section id="about-me">
+      <h2 className="text-center text-sm-left">About Me</h2>
+      <img src={photo} className="d-block mx-auto mr-sm-2 float-sm-left" alt="Chris Southwick" />
       <div className="my-2">
         <p>As a web developer with over five years of experience, I initially dipped my toes into web development during a project course at UMass Lowell while earning my bachelor's degree in computer science. I was instantly hooked and sought to create a new website for <a href="http://gctv.georgetownma.gov/">Georgetown Community Television</a>, learning PHP and SOAP along the way to interface with their database and scheduling server. As a result of this work, <a href="http://tightrope.townofrowley.org/RCM/">Rowley Community Media</a> brought me on to create a website for them from the same codebase with tweaks for their branding. Both sites are currently undergoing a major transition from the old SOAP API to a RESTful API thanks to new video servers with enhanced capabilities that include Video On Demand and live streaming.</p>
 
