73c69b638ed03a1230eff21611d13e2b9e07824d
Chris Southwick <csouthwick@live.com>
2025-03-06 18:12:10 -0500

    fix images and pdf

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,7 +6,7 @@ const Project = ({ project }) => {
     <div className="card col-md-6 border-0 mb-4">
       <img
         className="card-img-top"
-        src={require(`../../assets/images/${project.image}`).default}
+        src={require(`../../assets/images/${project.image}`)}
         alt={project.name}
         key={project.name}
       />
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,8 +3,8 @@ function Resume() {
   return (
     <section id="resume">
       <h2 className="text-center text-sm-left">Resume</h2>
-      <a href={require('../assets/Christopher-Southwick-Web-Developer.pdf').default}>Christopher-Southwick-Web-Developer.pdf</a>
-      <iframe src={require('../assets/Christopher-Southwick-Web-Developer.pdf').default} title="Resume"></iframe>
+      <a href={require('../assets/Christopher-Southwick-Web-Developer.pdf')}>Christopher-Southwick-Web-Developer.pdf</a>
+      <iframe src={require('../assets/Christopher-Southwick-Web-Developer.pdf')} title="Resume"></iframe>
     </section>
   );
 }
